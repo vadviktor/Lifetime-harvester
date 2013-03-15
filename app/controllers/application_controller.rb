605b24f2a1281db110c1efff5c76ff25ec9e3bca
Vad Viktor <vad.viktor@gmail.com>
2013-03-15 12:51:08 +0100

    cleanups

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    if(Rails.env.development?)
+      session[:user_id] = 1
+      @current_user ||= User.find(session[:user_id])
+    else
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
   end
 
   helper_method :current_user
